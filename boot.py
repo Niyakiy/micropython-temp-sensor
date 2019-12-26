7eb7e540a2eb4fa559473dd957d6e17ef071ae54
Eugene Chepurniy <eugene@zoomdata.com>
2019-12-26 11:26:38 +0200

    Refactored boot.py

--- boot.py
+++ boot.py
@@ -1,48 +1,79 @@
 # This is script that run when device boot up or wake from sleep.
 
 try:
-  import usocket as socket
+    import usocket as socket
 except:
-  import socket
+    import socket
 
 from machine import Pin, I2C
 import network
 import ubinascii
 import time
+import esp
 import dht
+import gc
+
 import bme280
 
-import esp
+# Get rid of debug and free some memory
 esp.osdebug(None)
-
-import gc
 gc.collect()
 
+# How many times to wait until WiFi connection
 CONNECT_RETRY_COUNT = 30
 
-ssid = 'Nest-Ext'
-password = 'divergence'
 
-station = network.WLAN(network.STA_IF)
+def get_wifi_creds(creds_file='wifi-credentials.txt'):
+    """
+    file should be in format: SSID\nPassword
 
-station.active(True)
-station.connect(ssid, password)
+    :param creds_file:
+    :return: list
+    """
 
-counter = 0
-while station.isconnected() == False:
-  print('.', end='')
-  time.sleep(0.5)
-  counter += 1
-  if counter > CONNECT_RETRY_COUNT:
-    break
+    try:
+        return open(creds_file, 'r').readlines()
+    except IOError as err:
+        print("Error loading WiFi credentials: ", err)
 
-print('Connection successful\nIP params:')
-print(station.ifconfig())
-print('MAC addr:')
-print(ubinascii.hexlify(station.config('mac'),':').decode())
+    return None
 
+
+def connect_to_wifi():
+    """
+    Connects to WiFi and waits until connection
+
+    :return: connection status
+    """
+
+    creds = get_wifi_creds()
+
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(creds[0], creds[1])
+
+    counter = 0
+    while not station.isconnected():
+        print('.', end='')
+        time.sleep(0.3)
+        counter += 1
+        if counter > CONNECT_RETRY_COUNT:
+            print("\nTimed out connection to {} WiFi network".format(creds[0]))
+            return False
+
+    print('Connected to {} WiFi network.\nIP params: {}\nMac: {}'.format(
+        creds[0],
+        station.ifconfig(),
+        ubinascii.hexlify(station.config('mac'), ':').decode()
+    ))
+    return True
+
+
+# Connect to WiFi
+connect_to_wifi()
+
+# Init globals to main.py
 ht = dht.DHT22(Pin(0, Pin.IN, Pin.PULL_UP))
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
 bme = bme280.BME280(i2c=i2c)
 led = Pin(2, Pin.OUT)
-
--- main.py
+++ main.py
@@ -1,67 +1,67 @@
 # This is your main script.
 
 def web_page():
-  if led.value() == 1:
-    gpio_state="ON"
-  else:
-    gpio_state="OFF"
-  
-  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+    if led.value() == 1:
+        gpio_state = "ON"
+    else:
+        gpio_state = "OFF"
+
+    html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
   h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
   border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
   .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
   <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
   <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
-  return html
+    return html
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
 while True:
-  conn, addr = s.accept()
-  print('Got a connection from %s' % str(addr))
-  request = str(conn.recv(1024))
-  print('Content = %s' % request)
-  led_on = request.find('/?led=on')
-  led_off = request.find('/?led=off')
-  temp = request.find('/temp')
-  if led_on == 6:
-    print('LED ON')
-    led.value(1)
-  if led_off == 6:
-    print('LED OFF')
-    led.value(0)
+    conn, addr = s.accept()
+    print('Got a connection from %s' % str(addr))
+    request = str(conn.recv(1024))
+    print('Content = %s' % request)
+    led_on = request.find('/?led=on')
+    led_off = request.find('/?led=off')
+    temp = request.find('/temp')
+    if led_on == 6:
+        print('LED ON')
+        led.value(1)
+    if led_off == 6:
+        print('LED OFF')
+        led.value(0)
 
-  if temp == 6:
-    retry = 0
-    while retry < 3:
-        try:
-            ht.measure()
-            break
-        except:
-            retry = retry + 1
-            print(".", end = "")
-            time.sleep(0.1)
+    if temp == 6:
+        retry = 0
+        while retry < 3:
+            try:
+                ht.measure()
+                break
+            except:
+                retry = retry + 1
+                print(".", end="")
+                time.sleep(0.1)
 
-    print("")
+        print("")
 
-
-    if retry < 3:
-        response = "<html><body>DHT T: {}<br>DHT H: {}<br><br>BME T: {}<br>BME H: {}<br>BME P: {}</body></html>".format(
-                ht.temperature(), 
+        if retry < 3:
+            response = "<html><body>DHT T: {}<br>DHT H: {}<br><br>BME T: {}<br>BME H: {}<br>BME P: {}</body></html>".format(
+                ht.temperature(),
                 ht.humidity(),
                 bme.temperature,
                 bme.humidity,
                 bme.pressure
-        )
-    else: 
-        response = "<html><body>Unable to load sensor data!</body></html>"
-  else:
-    response = web_page()
-  conn.send('HTTP/1.1 200 OK\n')
-  conn.send('Content-Type: text/html\n')
-  conn.send('Connection: close\n\n')
-  conn.sendall(response)
-  conn.close()
+            )
+        else:
+            response = "<html><body>Unable to load sensor data!</body></html>"
+    else:
+        response = web_page()
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
